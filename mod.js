721b683139c81ef5cc10871fa6800b22596a45b8
jonas.frey@unibe.ch <jonas.frey@unibe.ch>
2024-04-02 12:53:27 +0200

    added mit license generation

--- mod.js
+++ mod.js
@@ -652,6 +652,23 @@ let f_generate_template = async function(
             }
         )
     }
+    let s_full_name_for_mit_license = prompt(`for the MIT license, enter your full name:`);
+    await Deno.writeTextFile(
+        `${s_path_abs_folder}/LICENSE`,
+        `MIT License
+
+Copyright ${(new Date().getFullYear()).toString()} ${s_full_name_for_mit_license}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        `,
+        {
+            mode: 0o664
+        }
+    )
 
     console.log('done')
 
